9b553b023b43784a8a989961c1b253532a7d501a
Nipoon Sajjawiriyatham <nipoon123456789@gmail.com>
2022-04-08 23:02:53 +0700

    Initial commit

---
+++ script/1630900023.js
@@ -0,0 +1,19 @@
+// เพิ่มคลาส navbarDark บน navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// ยุบแถบนำทางหลังจากคลิกบนอุปกรณ์ขนาดเล็ก
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
